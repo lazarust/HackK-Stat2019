bd8377b9987ba60390a11847418e304d3959c93c
Victor Valdez <victorvaldez5@gmail.com>
2019-11-09 09:31:04 -0600

    add mongo

--- MinecraftApp/__init__.py
+++ MinecraftApp/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://yoda:theforce@0.0.0.0:27017/minecraftapp"
+mongo = PyMongo(app)
 
 from . import routes
