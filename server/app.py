17ac38eb62f8a79dafb608a9173767b9813db57f
Jake Herman <jake73@ksu.edu>
2019-11-09 14:08:38 -0600

    api changes

--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import request
 from flask_pymongo import PyMongo
+from flask_cors import CORS
+import os.path
+
 
 app = Flask(__name__)
+CORS(app, resources=r'/*')
 app.config["MONGO_URI"] = "mongodb://yoda:theforce@0.0.0.0:27017/minecraftapp"
 mongo = PyMongo(app)
 
@@ -20,59 +25,37 @@ def get_rgb_values(pixel_value):
     blue = pixel_value
     return [red,green,blue]
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
-    return 'bop'
+    file = os.join(root_dir)
+    return
 
 #minecraft block data
-@app.route('/colors/')
+@app.route('/colors/', methods=['GET'])
 def get_colors():
-    mc_resp = requests.get('http://minecraft-ids.grahamedgecombe.com/items.json')
-    block_data = json.loads(mc_resp.text)
-    color_block = {} #stores blocks by their most prominent color
-    count = 0
-    for block in block_data:
-        image = Image.open(f"static/{block['type']}-{block['meta']}.png")
-        rgb = image.convert('RGB')
+    f = open('new.json', 'r')
+    blocks = json.load(f)
+    color_block = {}
 
-        histogram = {} # keep track of prominent pixels
-
-        for i in range(0, 32): #block icons are 32x32
-            for j in range(0, 32):
-                pixel = rgb.getpixel((i, j))
-                if pixel != (0,0,0):
-                    pixel_val = pixel[0] + (256*pixel[1]) + (256*256*pixel[2]) #pixel value.. no longer rgb
-                    if pixel_val in histogram:
-                        histogram[pixel_val] += 1
-                    else:
-                        histogram[pixel_val] = 1
-        if histogram:
-            prominent_px = max(histogram, key = histogram.get) # most prominent pixel val for that block
-
-            if prominent_px in color_block:
-                color_block[prominent_px].append(block)
+    for block in blocks:
+        color = block['color']
+        if color is not None:
+            if color in color_block:
+                color_block[color].append(block)
             else:
-                color_block[prominent_px] = [block]
+                color_block[color] = [block]
 
-    # out = '<table><tr><th>color</th><th>blocks</th></tr>'
-    # for k in color_block:
-    #     out+=f'<tr><td>{k}</td><td><ul>'
-    #     for block in color_block[k]:
-    #         out+=f"<li><img src='/static/{block['type']}-{block['meta']}.png'/></li>"
-    #     out+='</ul></td></tr>'
-    #     print(k)
-    # out += '</table>'
-
-    out = ""
+    out = f"<img src='/static/17-1.png'/><br/>"
     pixels = []
-    image = Image.open('static/17-1.png')
-    rgb_im = image.convert('RGB')
+    block_info = request.args.get('block')
+    image = Image.open(f'static/{block_info}.png')
+    rgb_im = image.convert('RGBA')
 
     for i in range(0, 32):
         for j in range(0, 32):
             pixel = rgb_im.getpixel((i, j))
-            if pixel != (0,0,0):
-                r, g, b = pixel
+            if pixel != (0,0,0,0):
+                r, g, b, a = pixel
                 pixels.append([r, g, b])
     pixels.extend(["N", "N", "N"])
 
@@ -87,8 +70,10 @@ def get_colors():
     scheme = json.loads(resp.text)
 
     px_vals = list(color_block.keys())
+    json_response = {}
+
     for pixel in scheme['result']:
-        pixel_val = pixel[0] + (256*pixel[1]) + (256*256*pixel[2]) #pixel value.. no longer rgb
+        pixel_val = (pixel[0]) + (256*pixel[1]) + (256*256*pixel[2]) #pixel value.. no longer rgb
         if pixel_val in block:
             block = color_block[pixel_val]
             out+= f"<img src='/static/{block['type']}-{block['meta']}.png' style='border:2px solid {'#%02x%02x%02x' % tuple(get_rgb_values(pixel_val))}'/>"
@@ -96,10 +81,11 @@ def get_colors():
             #find key closest...
             closest = px_vals[min(range(len(px_vals)), key = lambda i: abs(px_vals[i]-pixel_val))]
             block = color_block[closest]
-            out+= f"<p style='background-color:{'#%02x%02x%02x' % tuple(get_rgb_values(pixel_val))}'><img src='/static/{block[0]['type']}-{block[0]['meta']}.png'/></p>"
+            out+= f"<p style='background-color:{'#%02x%02x%02x' % tuple(get_rgb_values(closest))}'><img src='/static/{block[0]['type']}-{block[0]['meta']}.png'/></p>"
 
+        json_response[f"{block[0]['type']}-{block[0]['meta']}"] = '#%02x%02x%02x' % tuple(get_rgb_values(pixel_val))
 
-    return out
+    return json.dumps(json_response)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="4000", debug=True)
