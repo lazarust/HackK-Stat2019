30e12c26688255d7dcafbeee9133b83a1a856118
Thomas Lazarus <tlazarus@ksu.edu>
2019-11-09 13:56:14 -0600

    Handle change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,34 +2,47 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Select from 'react-select';
+import { Container, Row, Col } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' },
 ];
-document.body.style = 'background: #202040;';
+document.body.style = 'background: #465881;';
 
 const App = () => {
   const [selected, setSelected] = useState(null);
 
   const handleChange = (selectedOption) => {
+      console.log(selectedOption)
     setSelected({ selectedOption })
     console.log(`Option selected: ${selected}`)
   }
 
   return (
-      <div class="container">
-          <div class="row">
-              <div class="col align-self-center" stlye="color:#602080">
-                    <Select
-                      value={selected}
-                      onChange={handleChange}
-                      options={options}
-                    />
-               </div>
-           </div>
-      </div>
+      <Container>
+          <Row>
+              <Col sm="12" md={{ size: 6, offset: 3 }}>
+                <Select
+                  value={selected && selected.label}
+                  onChange={handleChange}
+                  options={options}
+                  isClearable={true}
+                  theme={theme => ({
+                      ...theme,
+                      borderRadius: 0,
+                      colors: {
+                        ...theme.colors,
+                        primary25: 'grey',
+                        primary: '#00909e',
+                      },
+                    })}
+                />
+               </Col>
+           </Row>
+      </Container>
   );
 }
 
